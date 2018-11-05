fd8657b7ed14424d085580e1720412a50d468328
JonrePienaar <jonre.pienaar14@gmail.com>
2018-11-05 10:11:44 +0200

    firstcommit

---
+++ main.js
@@ -0,0 +1,6 @@
+// $(window).on('resize', function() {
+//     if($(window).width() > 605) {
+//         $('#xs').addClass('col-md-12');    }else{
+//         $('#xs').removeClass('col-md-12');
+//     }
+// })
